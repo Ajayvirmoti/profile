401dd6aec52ab53df489e468d0fde6f5a7865f77
Ajayvirmoti <email.ajaysingh01@gmail.com>
2024-01-27 04:51:21 +0530

    favicon

--- src/Components/Inventory/Others/Others.js
+++ src/Components/Inventory/Others/Others.js
@@ -14,16 +14,11 @@ const Others = () => {
                     <Card className="others__card">
                         <img src={require("../../Assets/img/" + data.card.logo)} className="project__banner" />
                         <div className="project__card__title">
-                            {console.log(data.card)}
                             <h1 className="project__title">{data.card.title}</h1>
                             <div className="project__description">{data.card.about}</div>
                         </div>
-                    </Card>
-
-
-
+                    </Card> 
                 ))}
-                {/* <div>hello</div> */}
             </div>
         </div>
     )
